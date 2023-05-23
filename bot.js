313146a0a58ef1e59360098f89611f1e07cdc0bb
Mace-X6 <mees@vansteijn.info>
2023-05-23 09:27:41 +0200

    integrating arguments and commandhandler

--- bot.js
+++ bot.js
@@ -1,13 +1,41 @@
-const Discord = require("discord.js");
-const fs = require("fs");
+const Discord = require('discord.js');
+const fs = require('fs');
+const { token } = require('./config.json');
+const myArgs = process.argv.slice(2);
 const client = new Discord.Client({
-    intents: new Discord.Intents(46603)
+    intents: 130571
 });
+const slashCommandFactory = require('./slashCommandFactory.js');
+const commandHandler = require('./commandHandler');
 
-client.on("messageCreate", (message) => {
-    if (message.content === "!ping") {
-        message.reply("Pong!");
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    checkArgs();
+
+    async function checkArgs() {
+        if (myArgs.includes('refreshCommands') && myArgs.includes('guild')) {
+            new slashCommandFactory(client, '512952764431859713');
+        }
+        else if (myArgs.includes('refreshCommands')) {
+            new slashCommandFactory(client);
+        }
+        else if (myArgs.includes('help')) {
+            console.log('refreshCommands: refreshes commands');
+            console.log('refreshCommands guild: refreshes commands for a specific guild');
+        }
     }
 });
 
-client.login();
+client.on('messageCreate', (message) => {
+
+});
+
+client.on('interactionCreate', async (interaction) => {
+    commands = JSON.parse(fs.readFileSync("./commands.json")).map(command => {return command.name});
+    if (!interaction.isCommand() || !commands.includes(interaction.commandName)) return;
+
+    new commandHandler(interaction);
+
+});
+
+client.login(token);
