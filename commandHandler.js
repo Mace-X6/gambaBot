21fa5fcde839d5cddb088f8275a1c73fb95d7aa9
Mace-X6 <mees@vansteijn.info>
2023-05-23 13:25:41 +0200

    moved check for if user has risen and grinded to commandHandler

--- commandHandler.js
+++ commandHandler.js
@@ -2,7 +2,18 @@ class commandHandler {
 
     constructor(interaction) {
         this.interaction = interaction;
-        this.handleCommand();
+        var userData = JSON.parse(fs.readFileSync("./userData.json"));
+        if (userData.includes(user => user.id === this.interaction.user.id)) {
+            if (userData.includes(user => user.id === this.interaction.options.filter(option => { return option.name === 'user' })[0].user.id)) {
+                this.handleCommand();
+            }
+            else {
+                this.interaction.reply({ content: `<@${this.interaction.options.filter(option => { return option.name === 'user' })[0].user.id}> hasn't risen and grinded yet!`, ephemeral: true });
+            }
+        }
+        else {
+            this.interaction.reply({content: `You haven't risen and grinded yet!`, ephemeral: true});
+        }
     }
     handleCommand() {
         var command = require(`./commands/${this.interaction.commandName}.js`);
