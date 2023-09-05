618f5d58efa2c6cf73600c869763ba6bd113f879
Mace-X6 <mees@vansteijn.info>
2023-09-05 11:49:25 +0200

    turned off commands and added reactions to big wins/losses

--- bot.ts
+++ bot.ts
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const fs : any = require('fs');
+const fs: any = require('fs');
 const token: string = require('./config.json').token;
 const myArgs = process.argv.slice(2);
 const client = new Discord.Client({
@@ -13,34 +13,53 @@ interface User {
     id: string,
     xp: number,
     balance: number,
-    begTimeOut: number    
+    begTimeOut: number
 }
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    checkArgs();
+    // checkArgs();
 
-    async function checkArgs() {
-        if (myArgs.includes('refreshCommands')) {
-            console.log("refreshing commands...");
-            new slashCommandFactory(client);
-        }
-        else if (myArgs.includes('help')) {
-            console.log('refreshCommands: refreshes commands');
-        }
-    }
+    // async function checkArgs() {
+    //     if (myArgs.includes('refreshCommands')) {
+    //         console.log("refreshing commands...");
+    //         new slashCommandFactory(client);
+    //     }
+    //     else if (myArgs.includes('help')) {
+    //         console.log('refreshCommands: refreshes commands');
+    //     }
+    // }
 });
 
 client.on('messageCreate', (message: any) => {
     if (message.author.bot) return;
-    message.content.includes('🤑') || message.content.includes('💰') || message.content.includes('💸') ? message.reply('🤑'.repeat(Math.random() * 100)) : null;
+    else {
+        message.content.includes('🤑') || message.content.includes('💰') || message.content.includes('💸') ? message.reply('🤑'.repeat(Math.random() * 100)) : null;
+    }
+});
+client.on('messageUpdate', (oldMessage: any, newMessage: any) => {
+    if (newMessage.author.id === '478202666355523585') {
+        if (newMessage.embeds[0]) {
+            let description = newMessage.embeds[0].description;
+            let loseRegex = /You lost ([\d,]+)/;
+            let winRegex = /You won ([\d,]+)/;
+            if (description.match(loseRegex)?.[1] > 10000) {
+                let random: number = Math.floor(Math.random() * 10);
+                newMessage.reply({ files: [{ attachment: `./gambas/sad/${random}.png`, name: 'sadgamba.png' }] });
+            }
+            if (description.match(winRegex)?.[1] > 10000) {
+                let random: number = Math.floor(Math.random() * 5);
+                newMessage.reply({ files: [{ attachment: `./gambas/happy/${random}.png`, name: 'happygamba.png' }] });
+            }
+        }
+    }
 });
 
-client.on('interactionCreate', async (interaction: any) => {
-    var commands = JSON.parse(fs.readFileSync("./commands.json")).map((command: any) => { return command.name });
-    if (!interaction.isCommand() || !commands.includes(interaction.commandName)) return;
-    await interaction.deferReply({ ephemeral: true });
-    new commandPipeline(interaction);
-});
+// client.on('interactionCreate', async (interaction: any) => {
+//     var commands = JSON.parse(fs.readFileSync("./commands.json")).map((command: any) => { return command.name });
+//     if (!interaction.isCommand() || !commands.includes(interaction.commandName)) return;
+//     await interaction.deferReply({ ephemeral: true });
+//     new commandPipeline(interaction);
+// });
 
 client.login(token);
